48a71af5fad7c1886b817f2a2d23ad729a48bdc9
Juan Casas <imhouses@ciencias.unam.mx>
2019-02-16 19:11:20 -0600

    Problem: Fibonacci Number using arrays.

---
+++ Problems/Arrays/FibonacciNumber.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+/**
+ * 509. Fibonacci Number. 
+ * Problem from LeetCode https://leetcode.com/problems/fibonacci-number/
+ * 
+ * This problem is in the array section because it can be solved using an array
+ * with the method Memoization, which is a method to save previous results if we
+ * need them later, saving computing time.
+ */
+class FibonacciNumber {
+
+    public static void main(String[] args) {
+        /* Generate input. */
+        int input = new Random().nextInt(31);
+        System.out.println("N = " + input);
+        int output = fib(input);
+        System.out.println("F(N) = " + output);
+    }
+
+    public static int fib(int N) {
+        int[] results = new int[N+1];
+        results[0] = 0;
+        results[1] = 1;
+        return fibAux(N, results);
+    }
+
+    /**
+     * Auxiliar function to return the Fibonacci of the given number N.
+     * @param N
+     * @param results
+     * @return
+     */
+    private static int fibAux(int N, int[] results) {
+        if(N == 0) {
+            return 0;
+        }
+        if(results[N] == 0) {
+            int res = fibAux(N-1, results) + fibAux(N-2, results);
+            results[N] = res;
+            return res;
+        } else {
+            return results[N];
+        }
+    }
+}
